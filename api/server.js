76b64fc07920dcbc22ba4e91b5ce22d9026fbe1c
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-04-07 10:09:34 -0700

    dependencies

--- api/server.js
+++ api/server.js
@@ -1,12 +1,16 @@
-const express = require('express');
-
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
+server.use(express.json());
+server.use(morgan("dev"));
+server.use(helmet());
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
